d1c6e11bb46e4af0ad0b06cf6dab928a3ae73a1c
Matthias Kern <matthias@nuc>
2024-10-15 15:59:25 +0200

    temperature threshold writeable through MQTT

--- mqtt_hotpot.ino
+++ mqtt_hotpot.ino
@@ -5,8 +5,9 @@
 #include <PubSubClient.h>
 
 #include "mqtt_hotpot.h"
+void lowpass(float *filter, float temp);
 
-const float temperature_change_threshold = 0.1;
+      float temperature_change_threshold = 0.2;
 const float freeze_threshold             = 2.0;
 const float fire_threshold               = 5.0;
 #define UPDATE_RATE_SECONDS 1
@@ -22,12 +23,16 @@ PubSubClient client(espClient);
 // MQTT Topics
 char mqtt_message[800];
 char topic[60];
-const char *state_topic = "homeassistant/sensor/sensor_hotpot/state";
+const char *temperature_state_topic = "homeassistant/sensor/sensor_hotpot/state";
+const char *fire_state_topic        = "homeassistant/binary_sensor/sensor_fire_hotpot/state";
+const char *freeze_state_topic      = "homeassistant/binary_sensor/sensor_freeze_hotpot/state";
 
 
 // Temperature sensors
-// Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS D4
+// Filter pump
+#define FILTER_RELAY D5
+unsigned long filterLastOnMillis = 0;
 
 // Setup a OneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
@@ -37,10 +42,21 @@ DallasTemperature sensors(&oneWire);
 
 float temp_vor = 0.0, temp_rueck = 0.0, temp_vor_prev = 0.0, temp_rueck_prev = 0.0, temp_difference = -5.0;
 float vor_change = 0.0, rueck_change = 0.0;
+float vorlauf_filter[5];
 unsigned long previousMillis;
 
 void mqtt_callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("Received MQTT message");
+  // for (int i = 0; i < length; i++)
+  // {
+  //     Serial.print((char)payload[i] );
+  // }
+  if (topic, String(String(mqtt_topic) + "/temp_threshold"))
+  {
+     Serial.print("Updating temperature threshold to ");
+     temperature_change_threshold = (float)atof((const char *)(payload));
+     Serial.println(temperature_change_threshold);
+  }
 }
 
 void setup(void) {
@@ -98,6 +114,7 @@ void setup(void) {
     // Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("Public EMQX MQTT broker connected");
+      client.subscribe("hotpot");
     } else {
       Serial.print("failed with state ");
       Serial.print(client.state());
@@ -113,19 +130,19 @@ void setup(void) {
   }
 
   // Create Vorlauf Sensor
-  createNewSensor("Vorlauf",   "temperature", "homeassistant/sensor/sensor_hotpot/state", "unique_id_vor",   "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "°C", "{{ value_json.vorlauf }}");
+  createNewSensor("Vorlauf",   "temperature", temperature_state_topic, "unique_id_vor",   "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.vorlauf }}");
 
   // Create Ruecklauf Sensor
-  createNewSensor("Rücklauf",  "temperature", "homeassistant/sensor/sensor_hotpot/state", "unique_id_rueck", "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "°C", "{{ value_json.ruecklauf }}");
+  createNewSensor("Rücklauf",  "temperature", temperature_state_topic, "unique_id_rueck", "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.ruecklauf }}");
 
   // Create Difference Sensor
-  createNewSensor("Differenz", "temperature", "homeassistant/sensor/sensor_hotpot/state", "unique_id_diff",  "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "°C", "{{ value_json.difference }}");
+  createNewSensor("Differenz", "temperature", temperature_state_topic, "unique_id_diff",  "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.difference }}");
 
   // Create Fire state Sensor
-  createNewSensor("Feuer",     "heat",        "homeassistant/binary_sensor/sensor_fire_hotpot/state", "unique_id_fire_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
+  createNewSensor("Feuer",     "heat",        fire_state_topic,        "unique_id_fire_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
 
   // Create Freeze state Sensor
-  createNewSensor("Frost",     "cold",        "homeassistant/binary_sensor/sensor_freeze_hotpot/state", "unique_id_freeze_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
+  createNewSensor("Frost",     "cold",        freeze_state_topic,      "unique_id_freeze_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
 
   if (client.setBufferSize(256)) {
     Serial.println("Resized buffer successfully");
@@ -172,11 +189,6 @@ void loop(void) {
      temp_rueck   = sensors.getTempCByIndex(1);
      vor_change   = temp_vor_prev   - temp_vor;
      rueck_change = temp_rueck_prev - temp_rueck;
-     Serial.print(temp_vor_prev);
-     Serial.print(" ");
-     Serial.print(temp_vor);
-     Serial.print(" ");
-     Serial.println(fabs(vor_change));
 
      // Update Temperatures if significant change
      if ( (fabs(vor_change) >= temperature_change_threshold) || (fabs(rueck_change) >= temperature_change_threshold) )
@@ -188,24 +200,25 @@ void loop(void) {
         temp_difference = temp_rueck - temp_vor;
         // Publish temperatures
         sprintf(mqtt_message, "{\"vorlauf\": %.2f, \"ruecklauf\": %.2f , \"difference\": %.2f}", temp_vor, temp_rueck, temp_difference);
-        client.publish(state_topic, mqtt_message);
+        client.publish(temperature_state_topic, mqtt_message);
         Serial.println(mqtt_message);
 
         // Publish Fire state
         if (temp_difference > fire_threshold) {
-          client.publish("homeassistant/binary_sensor/sensor_fire_hotpot/state", "ON");
+          client.publish(fire_state_topic, "ON");
         } else {
-          client.publish("homeassistant/binary_sensor/sensor_fire_hotpot/state", "OFF");
+          client.publish(fire_state_topic, "OFF");
         }
 
         // Publish Freeze state
         if ((temp_rueck < freeze_threshold) | (temp_vor < freeze_threshold)) {
-          client.publish("homeassistant/binary_sensor/sensor_freeze_hotpot/state", "ON");
+          client.publish(freeze_state_topic, "ON");
         } else {
-          client.publish("homeassistant/binary_sensor/sensor_freeze_hotpot/state", "OFF");
+          client.publish(freeze_state_topic, "OFF");
         }
      }
   }
+  lowpass(vorlauf_filter, temp_vor);
 
 }
 
@@ -229,15 +242,14 @@ void createNewSensor(const char *name,
                      const char *model, 
                      const char *model_id,
                      const char *sensor_type,
-                     const char *unit,
                      const char *value_template)
 {
   // Create MQTT payload
   if (sensor_type == "sensor")
   {
      sprintf(mqtt_message, 
-             "{\"name\": \"%s\", \"device_class\": \"%s\", \"state_topic\": \"%s\", \"unit_of_measurement\":\"%s\", \"value_template\":\"%s\", \"unique_id\": \"%s\", \"device\": {\"identifiers\": [\"%s\"], \"name\": \"%s\", \"manufacturer\": \"%s\", \"model\": \"%s\", \"model_id\": \"%s\"} }",\
-             name, device_class, state_topic, unit, value_template, unique_id, identifiers, device_name, manufacturer, model, model_id);
+             "{\"name\": \"%s\", \"device_class\": \"%s\", \"state_topic\": \"%s\", \"unit_of_measurement\":\"°C\", \"value_template\":\"%s\", \"unique_id\": \"%s\", \"device\": {\"identifiers\": [\"%s\"], \"name\": \"%s\", \"manufacturer\": \"%s\", \"model\": \"%s\", \"model_id\": \"%s\"} }",\
+             name, device_class, state_topic, value_template, unique_id, identifiers, device_name, manufacturer, model, model_id);
   } else {
      sprintf(mqtt_message, 
              "{\"name\": \"%s\", \"device_class\": \"%s\", \"state_topic\": \"%s\", \"unique_id\": \"%s\", \"device\": {\"identifiers\": [\"%s\"], \"name\": \"%s\", \"manufacturer\": \"%s\", \"model\": \"%s\", \"model_id\": \"%s\"} }",\
@@ -251,4 +263,23 @@ void createNewSensor(const char *name,
 
   // Publish
   client.publish(topic, mqtt_message,true);
+}
+
+
+void lowpass(float *filter, float temp)
+{
+   unsigned int length = sizeof(filter);
+   for (int i = length; i > 0; i--)
+   {
+      filter[i] = filter[i-1];
+   }
+   filter[0] = temp;
+
+   float output = 0.0;
+   for (int i = 0; i < length; i++)
+   {
+      output = output + filter[i];
+   }
+   output = output / length;
+  
 }
