eb892541428f842c589710b1bfff15eb7098b654
Matthias Kern <matthias@nuc>
2024-10-18 13:24:53 +0200

    Cleanup separate files for MQTT callback handling Waterfilter control

--- mqtt_hotpot.ino
+++ mqtt_hotpot.ino
@@ -1,10 +1,14 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
+//#include <WiFi.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 
 #include "mqtt_hotpot.h"
+#include "mqtt.h"
+#include "filter.h"
+
 void lowpass(float *filter, float temp);
 
       float temperature_change_threshold = 0.2;
@@ -14,25 +18,30 @@ const float fire_threshold               = 5.0;
 
 
 // MQTT Broker
-const char *topic_prefix  = "homeassistant/";
+const char *topic_prefix  = "homeassistant";
 const char *config_suffix = "/config";
 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 // MQTT Topics
-char mqtt_message[800];
+char mqtt_message[512];
 char topic[60];
 const char *temperature_state_topic = "homeassistant/sensor/sensor_hotpot/state";
 const char *fire_state_topic        = "homeassistant/binary_sensor/sensor_fire_hotpot/state";
 const char *freeze_state_topic      = "homeassistant/binary_sensor/sensor_freeze_hotpot/state";
+const char *filter_state_topic      = "homeassistant/binary_sensor/sensor_filter_hotpot/state";
 
 
 // Temperature sensors
 #define ONE_WIRE_BUS D4
+//#define ONE_WIRE_BUS 26
+
 // Filter pump
-#define FILTER_RELAY D5
-unsigned long filterLastOnMillis = 0;
+#define FILTER_RELAY 16
+unsigned long filterOnSince = 0, filterOffSince = 0;
+unsigned int filterIntervalHours = 6;
+unsigned int filterDurationMinutes = 30;
 
 // Setup a OneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
@@ -40,24 +49,16 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass OneWire reference to Dallas Temperature DallasTemperature
 DallasTemperature sensors(&oneWire);
 
-float temp_vor = 0.0, temp_rueck = 0.0, temp_vor_prev = 0.0, temp_rueck_prev = 0.0, temp_difference = -5.0;
+#define HISTORY_LENGTH 6
+float temp_vor[HISTORY_LENGTH] = { }, temp_rueck[HISTORY_LENGTH] = { }, temp_difference = -5.0;
+float temp_vor_last_transmitted = 0.0, temp_rueck_last_transmitted = 0.0;
+// Tendency calculation
+float temp_vor_avg = 0.0, temp_rueck_avg = 0.0;
 float vor_change = 0.0, rueck_change = 0.0;
-float vorlauf_filter[5];
-unsigned long previousMillis;
 
-void mqtt_callback(char *topic, byte *payload, unsigned int length) {
-  Serial.println("Received MQTT message");
-  // for (int i = 0; i < length; i++)
-  // {
-  //     Serial.print((char)payload[i] );
-  // }
-  if (topic, String(String(mqtt_topic) + "/temp_threshold"))
-  {
-     Serial.print("Updating temperature threshold to ");
-     temperature_change_threshold = (float)atof((const char *)(payload));
-     Serial.println(temperature_change_threshold);
-  }
-}
+float vorlauf_filter[HISTORY_LENGTH];
+unsigned long previousMillis;
+bool fire = false;
 
 void setup(void) {
   // Connect to serial interface
@@ -109,47 +110,39 @@ void setup(void) {
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(mqtt_callback);
   while (!client.connected()) {
-    String client_id = "hotpot_mqtt-client";
-    //client_id += String(WiFi.macAddress(mac));
-    // Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("Public EMQX MQTT broker connected");
-      client.subscribe("hotpot");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
+     connectMQTT();
   }
 // -------------------------------------------------------------------------
 
 
   // Increase MQTT Buffer size
-  if (client.setBufferSize(512)) {
-    Serial.println("Resized buffer successfully");
-  }
+  client.setBufferSize(512);
 
   // Create Vorlauf Sensor
-  createNewSensor("Vorlauf",   "temperature", temperature_state_topic, "unique_id_vor",   "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.vorlauf }}");
+  createNewSensor("Vorlauf",   "temperature", temperature_state_topic, "ui_vor",   "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.vorlauf }}");
 
   // Create Ruecklauf Sensor
-  createNewSensor("Rücklauf",  "temperature", temperature_state_topic, "unique_id_rueck", "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.ruecklauf }}");
+  createNewSensor("Rücklauf",  "temperature", temperature_state_topic, "ui_rueck", "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.ruecklauf }}");
 
   // Create Difference Sensor
-  createNewSensor("Differenz", "temperature", temperature_state_topic, "unique_id_diff",  "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.difference }}");
+  createNewSensor("Differenz", "temperature", temperature_state_topic, "ui_diff",  "hotpot_temperature", device_name, manufacturer, model, model_id, "sensor", "{{ value_json.difference }}");
 
   // Create Fire state Sensor
-  createNewSensor("Feuer",     "heat",        fire_state_topic,        "unique_id_fire_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
+  createNewSensor("Feuer",     "heat",        fire_state_topic,        "ui_fire_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor", "{{ value_json.fire }}");
 
   // Create Freeze state Sensor
-  createNewSensor("Frost",     "cold",        freeze_state_topic,      "unique_id_freeze_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor");
+  createNewSensor("Frost",     "cold",        freeze_state_topic,      "ui_freeze_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor", "{{ value_json.freeze }}");
 
-  if (client.setBufferSize(256)) {
-    Serial.println("Resized buffer successfully");
-  }
+  // Create Filter state Sensor
+  createNewSensor("Filter",    "running",     filter_state_topic,      "ui_filter_state", "hotpot_temperature", device_name, manufacturer, model, model_id, "binary_sensor", "{{ value_json.filter }}");
+
+  client.setBufferSize(256);
 
   // Connect to sensors
+  pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
   sensors.begin();
+
+  pinMode(FILTER_RELAY, OUTPUT);
 }
 
 void loop(void) {
@@ -163,73 +156,145 @@ void loop(void) {
     connectWifi();
   }
 
+// -------------------------------------------------------------
+// TEMPERATURES
+// -------------------------------------------------------------
   if ((millis() - previousMillis) > (UPDATE_RATE_SECONDS*1000))
   {
+    // digitalWrite(FILTER_RELAY, LOW);
+    // delay(1000);
+    // digitalWrite(FILTER_RELAY, HIGH);
+    // delay(1000);
+    // digitalWrite(FILTER_RELAY, LOW);
+    // delay(1000);
+    // digitalWrite(FILTER_RELAY, HIGH);
     // Check MQTT connection
      if (!client.connected())
      {
         //reconnect();
-        String client_id = "hotpot_mqtt-client";
-        while (!client.connected()) {
-          if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-            Serial.println("Public EMQX MQTT broker connected");
-          } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-          }
-        }
+        connectMQTT();
      }
      // Store timestamp
      previousMillis = millis();
 
+     // Tendency
+     temp_vor_avg   = 0.0;
+     temp_rueck_avg = 0.0;
+     for (int i = 0; i < HISTORY_LENGTH; i++)
+     {
+        temp_vor_avg   += temp_vor[i];
+        temp_rueck_avg += temp_rueck[i];
+     }
+     temp_vor_avg   /= HISTORY_LENGTH;
+     temp_rueck_avg /= HISTORY_LENGTH;
+
      // Get temperature updates
      sensors.requestTemperatures();
-     temp_vor     = sensors.getTempCByIndex(0);
-     temp_rueck   = sensors.getTempCByIndex(1);
-     vor_change   = temp_vor_prev   - temp_vor;
-     rueck_change = temp_rueck_prev - temp_rueck;
+     for (int i = HISTORY_LENGTH-1; i > 0; i--)
+     {
+        temp_vor[i]   = temp_vor[i-1];
+        temp_rueck[i] = temp_rueck[i-1];
+     }
+     temp_vor[0]   = sensors.getTempCByIndex(0);
+     temp_rueck[0] = sensors.getTempCByIndex(1);
+     vor_change    = temp_vor[0]   - temp_vor_last_transmitted;
+     rueck_change  = temp_rueck[0] - temp_rueck_last_transmitted;
+
+//DEBUG
+     // for (int i = 0; i < HISTORY_LENGTH; i++)
+     // {
+     //    Serial.print(temp_vor[i]);
+     //    Serial.print(" ");
+     // }
+     //Serial.println("");
+     // Serial.println(temp_vor[0]);
+     // Serial.println(temp_rueck[0]);
+     // Serial.println(vor_change);
+     // Serial.println(rueck_change);
+     // if (vor_change > 0) {
+     //    Serial.println("Vorlauf temperature rising");
+     // } else {
+     //    Serial.println("Vorlauf temperature falling");
+     // }
+     // 
+     // if (rueck_change > 0) {
+     //    Serial.println("Ruecklauf temperature rising");
+     // } else {
+     //    Serial.println("Ruecklauf temperature falling");
+     // }
+     
 
      // Update Temperatures if significant change
      if ( (fabs(vor_change) >= temperature_change_threshold) || (fabs(rueck_change) >= temperature_change_threshold) )
      {
-        // Store temperatures
-        temp_vor_prev   = temp_vor;
-        temp_rueck_prev = temp_rueck;
         // Calculate difference
-        temp_difference = temp_rueck - temp_vor;
+        temp_difference = temp_rueck[0] - temp_vor[0];
+
         // Publish temperatures
-        sprintf(mqtt_message, "{\"vorlauf\": %.2f, \"ruecklauf\": %.2f , \"difference\": %.2f}", temp_vor, temp_rueck, temp_difference);
+        sprintf(mqtt_message, "{\"vorlauf\": %.1f, \"ruecklauf\": %.1f , \"difference\": %.1f}", temp_vor[0], temp_rueck[0], temp_difference);
         client.publish(temperature_state_topic, mqtt_message);
+        temp_vor_last_transmitted   = temp_vor[0];
+        temp_rueck_last_transmitted = temp_rueck[0];
         Serial.println(mqtt_message);
 
         // Publish Fire state
-        if (temp_difference > fire_threshold) {
-          client.publish(fire_state_topic, "ON");
-        } else {
-          client.publish(fire_state_topic, "OFF");
-        }
+        fire = temp_difference > fire_threshold;
+        updateBinarysensor(fire_state_topic, fire);
 
         // Publish Freeze state
-        if ((temp_rueck < freeze_threshold) | (temp_vor < freeze_threshold)) {
-          client.publish(freeze_state_topic, "ON");
-        } else {
-          client.publish(freeze_state_topic, "OFF");
-        }
+        updateBinarysensor(freeze_state_topic, ((temp_rueck[0] < freeze_threshold) | (temp_vor[0] < freeze_threshold)));
      }
   }
-  lowpass(vorlauf_filter, temp_vor);
+  // lowpass(vorlauf_filter, temp_vor);
 
-}
+// -------------------------------------------------------------
+// FILTER
+// -------------------------------------------------------------
+ //  if (fire)
+ //  {
+ //  // Turn off filter
+ //    switchFilter(false);
+ //  }
+ //  if ((millis() - filterOffSince) > (filterIntervalHours*60*60*1000))
+ //  {
+ //    // Turn on filter
+ //    switchFilter(true);
+ //  }
+ //  if ((millis() - filterOnSince) > (filterDurationMinutes*60*1000))
+ //  {
+ //    // Turn off filter
+ //    switchFilter(false);
+ //  }
+} 
 
 void connectWifi()
 {
-  Serial.print("Connecting to WiFi... ");
+  Serial.print("Connecting to WiFi...");
   WiFi.begin((char *)ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
   } 
   Serial.println("... connected");
+}
+
+void connectMQTT()
+{
+   String client_id = "hotpot_mqtt-client";
+   Serial.print("Connecting to MQTT broker...");
+   while (!client.connected()) {
+     Serial.print(".");
+     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+       client.subscribe("hotpot/temp_threshold");
+       client.subscribe("hotpot/filter_duration");
+       client.subscribe("hotpot/filter");
+       Serial.println("connected");
+     } else {
+       Serial.print("failed with state ");
+       Serial.print(client.state());
+       delay(2000);
+     }
+   }
 }
 
 void createNewSensor(const char *name,
@@ -248,7 +313,7 @@ void createNewSensor(const char *name,
   if (sensor_type == "sensor")
   {
      sprintf(mqtt_message, 
-             "{\"name\": \"%s\", \"device_class\": \"%s\", \"state_topic\": \"%s\", \"unit_of_measurement\":\"°C\", \"value_template\":\"%s\", \"unique_id\": \"%s\", \"device\": {\"identifiers\": [\"%s\"], \"name\": \"%s\", \"manufacturer\": \"%s\", \"model\": \"%s\", \"model_id\": \"%s\"} }",\
+             "{\"name\": \"%s\", \"device_class\": \"%s\", \"state_topic\": \"%s\", \"unit_of_measurement\": \"°C\", \"value_template\": \"%s\", \"unique_id\": \"%s\", \"device\": {\"identifiers\": [\"%s\"], \"name\": \"%s\", \"manufacturer\": \"%s\", \"model\": \"%s\", \"model_id\": \"%s\"} }",\
              name, device_class, state_topic, value_template, unique_id, identifiers, device_name, manufacturer, model, model_id);
   } else {
      sprintf(mqtt_message, 
@@ -258,13 +323,22 @@ void createNewSensor(const char *name,
   // Create MQTT topic
   sprintf(topic, "%s/%s/%s/config", topic_prefix, sensor_type, unique_id);
 
-  Serial.print("Creating sensor: ");
-  Serial.println(mqtt_message);
+  // Serial.print("Creating sensor: ");
+  // Serial.println(topic);
+  // Serial.println(mqtt_message);
 
   // Publish
-  client.publish(topic, mqtt_message,true);
+  client.publish(topic, mqtt_message, true);
 }
 
+void updateBinarysensor(const char* topic, bool state)
+{
+        if (state) {
+          client.publish(topic, "ON");
+        } else {
+          client.publish(topic, "OFF");
+        }
+}
 
 void lowpass(float *filter, float temp)
 {
@@ -283,3 +357,4 @@ void lowpass(float *filter, float temp)
    output = output / length;
   
 }
+
